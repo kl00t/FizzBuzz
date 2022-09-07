1815f1eb8e0c327162feec774420499aa45f001d
Scott Vaughan <scott.vaughan@ensono.com>
2022-09-07 14:02:15 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿int iterations = 100;
+for (int i = 1; i < iterations; i++)
+{
+    var output = string.Empty;
+
+    if (i % 3 == 0)
+    {
+        output += "Fizz";
+    }
+    if (i % 5 == 0)
+    {
+        output += "Buzz";
+    }
+
+    if (output == string.Empty)
+    {
+        output = i.ToString();
+    }
+
+    Console.WriteLine(output);
+}
